729ec54da652788febbaf31bc4a5f3c610badcfe
Kevin Park <kevin@fruitful.fi>
2021-11-10 18:40:40 -0500

    test heroku

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn meta() -> &'static str {
+    "Hello! The Fruitful Metaverse is under construction. Yes, I can't wait to play either"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![meta])
 }
